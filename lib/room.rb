3829f5f3a62dbb9a992de3f7f31676c14b1ca1a4
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-03-29 08:25:16 -0700

    updated Room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,21 @@
 class Room
   attr_accessor :title, :date_created, :price, :url
+
+  def self.create_from_hash(hash) #Instantiating and saving
+    new_from_hash(hash).save
+  end
+
+  def self.new_from_hash(hash) #Just Instantiating
+    room = self.new
+    room.title = hash[:title]
+    room.date_created = hash[:date_created]
+    room.price = hash[:price]
+    room.url = hash[:url]
+    binding.pry
+    room
+  end
+
+  def save
+    puts "You are saving #{self}"
+  end
 end
